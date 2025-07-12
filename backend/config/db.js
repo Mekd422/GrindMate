7b031deb0a5069070ccdce0aeb80d900dc8a7bcc
Mekd422 <mekdes.asefa-ug@aau.edu.et>
2025-07-12 17:42:53 +0300

    initial commit_:)

---
+++ backend/config/db.js
@@ -0,0 +1,16 @@
+// config/db.js
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.DATABASE_URL);
+    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.error(`❌ MongoDB Connection Error: ${err.message}`);
+    process.exit(1); // Exit process with failure
+  }
+};
+
+module.exports = connectDB;
